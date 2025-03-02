315306fa284e286c1d6e5c079c62e839ead1edce
Muhammad <mufaisal2005@gmail.com>
2025-03-02 06:51:48 -0600

    fixing certain naming inconsistencies

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,11 +10,10 @@ const Home = () => {
       <div style={styles.content}>
         <div style={styles.logoContainer}>
           <img src="commitcoin.png" alt="Commit Coin" style={styles.logo} />
-          <h1 style={styles.websiteName}>Commit Coin</h1>
         </div>
-        <h1 style={styles.heading}>Welcome to Hot Plates!</h1>
+        <h1 style={styles.heading}>Welcome to Commit Coin!</h1>
         <button style={styles.button} onClick={handleButtonClick}>
-          Begin !
+          Begin committing!
         </button>
       </div>
     </div>
@@ -29,7 +28,7 @@ const styles = {
     alignItems: "center",
     height: "100vh",
     width: "189vh",
-    backgroundImage: "url('your-background-image.jpg')", // Replace with your background image
+    backgroundImage: "url('your-background-image.jpg')", 
     backgroundSize: "cover",
     backgroundPosition: "center",
     fontFamily: "Avenir ",
@@ -38,11 +37,11 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    backgroundColor: "rgba(26, 26, 46, 0.8)", // Semi-transparent background for content
+    backgroundColor: "rgba(26, 26, 46, 0.8)", 
     padding: "4rem",
     borderRadius: "12px",
     boxShadow: "0 8px 20px rgba(0, 0, 0, 0.2)",
-    backdropFilter: "blur(10px)", // Add a subtle blur effect
+    backdropFilter: "blur(10px)", 
     height: "900em",
     width:"100em",
   },
@@ -50,27 +49,27 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    marginBottom: "3rem",
+    marginBottom: "2rem",
     animation: "fadeInUp 1s ease", 
   },
   logo: {
-    width: "150px",
-    height: "150px",
-    marginBottom: "1rem",
+    width: "180px",
+    height: "180px",
+    marginBottom: "-1rem",
     animation: "rotate 2s linear infinite", 
     borderRadius: "10%",
   },
   websiteName: {
-    fontSize: "2.5rem",
+    fontSize: "3rem",
     color: "#FFFFFF",
     fontWeight: "bold",
-    animation: "fadeInUp 1s ease", // Add a fade-in-up animation
+    animation: "fadeInUp 1s ease", 
   },
   heading: {
     fontSize: "4rem",
     color: "#FFFFFF",
-    marginBottom: "2rem",
-    animation: "fadeInUp 1s ease 0.5s", // Add a delayed fade-in-up animation
+    marginBottom: "3rem",
+    animation: "fadeInUp 1s ease 0.5s", 
   },
   button: {
     padding: "12px 40px",
@@ -80,7 +79,7 @@ const styles = {
     borderRadius: "6.5px",
     cursor: "pointer",
     fontSize: "1.5rem",
-    animation: "fadeInUp 1s ease 1s", // Add a delayed fade-in-up animation
+    animation: "fadeInUp 1s ease 1s", 
   },
   // Keyframes for animations
   "@keyframes fadeInUp": {
